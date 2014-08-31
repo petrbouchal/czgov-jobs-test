0fd2bd78e2869f21fa0e8f6745dc833492079041
Petr Bouchal <pbouchal@gmail.com>
2014-08-31 18:17:00 +0100

    outline of local scraper - additionally download and store whole pages

--- scraper_local.py
+++ scraper_local.py
@@ -1,1 +1,9 @@
 __author__ = 'petrbouchal'
+
+execfile('./scraper.py')
+
+# read back saved links
+
+# download each job file and save into another table in the same local db - URL + date as ID
+
+# download each job list file and save into yet another table in the same local db - date + dept as ID
